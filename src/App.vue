0a23c24419db455e66219852916b29f4a6fcc5e2
s11o-4 <rbnsilo@gmail.com>
2025-04-21 02:29:58 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <main class="min-h-screen min-w-full">
+    <HelloWorld />
+  </main>
+</template>
+
+<style scoped>
+</style>
